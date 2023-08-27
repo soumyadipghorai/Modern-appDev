3911d4ae60a1a2438e31121f6e33a5aa65e0c362
Soumyadip Ghorai <ghorai.soumyadip33@gmail.com>
2023-08-27 23:40:01 +0530

    flask_security added

---
+++ week10/example/application/config.py
@@ -0,0 +1,28 @@
+import os 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config() : 
+    DEBUG = False 
+    SQLITE_DB_DIR = None 
+    SQLALCHEMY_DATABASE_URI = None 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
+
+class LocalDevelopmentConfig(Config) : 
+    SQLITE_DB_DIR = os.path.join(basedir, "../db_directory")
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(SQLITE_DB_DIR, 'testdb.sqlite3')
+    DEBUG = True 
+    SECURITY_PASSWORD_HASH = 'bcrypt'
+    # ! read from env 
+    SECRET_KEY = "KJ!&&S18@^#73jhcbwic!@#^" # strong random difficult key 
+    SECURITY_PASSWORD_SALT = '289&#219KAJ@!*@Dach%^jb!!*@' #strong, unique, random key or salt
+    SECURITY_REGISTERABLE = True 
+    SECURITY_SEND_REGISTER_EMAIL = False # send automatic email
+    SECURITY_UNAUTHORIZED_VIEW = None # if its unauthorized through 403 error
+
+"""
+class ProductionDevelopmentConfig(Config) : 
+    SQLITE_DB_DIR = os.path.join(basedir, "../db_directory")
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(SQLITE_DB_DIR, 'proddb.sqlite3')
+    DEBUG = False 
+    PASSWORD = os.getenv('PASSWORD')
+"""
